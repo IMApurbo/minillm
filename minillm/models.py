2f78576693226b15774e2143a885de7497c48913
Akm Korishee Apurbo <155003446+IMApurbo@users.noreply.github.com>
2024-11-16 15:54:57 +0600

    Update models.py

--- minillm/models.py
+++ minillm/models.py
@@ -1,24 +1,21 @@
 import re
+import os
 from huggingface_hub import hf_hub_download, snapshot_download
 from tokenizers import Tokenizer
 import ctranslate2
 
 from minillm.config import config, models
 
-
 modelcache = {}
-
 
 class ModelException(Exception):
     pass
 
+# Add your Hugging Face access token here
+access_token = os.getenv("HF_API_KEY")
 
 def get_model_info(model_type="instruct"):
-    """Gets info about the current model in use
-
-    >>> get_model_info('instruct')
-    {'name': 'LaMini-Flan-T5-248M', 'tuning': 'instruct'...
-    """
+    """Gets info about the current model in use"""
     model_name = config[f"{model_type}_model"]
 
     m = [m for m in models if m["name"] == model_name][0]
@@ -29,17 +26,20 @@ def get_model_info(model_type="instruct"):
     if "/" in m["name"]:
         m["path"] = m["name"]
     else:
-        m["path"] = f"jncraton/{m['name']}-{m['backend']}-{m['quantization']}"
+        m["path"] = f"verifybadge/{m['name']}-{m['backend']}-{m['quantization']}"
 
     return m
-
 
 def initialize_tokenizer(model_type, model_name):
     model_info = get_model_info(model_type)
     rev = model_info.get("revision", None)
 
     tok_config = hf_hub_download(
-        model_info["path"], "tokenizer.json", revision=rev, local_files_only=True
+        model_info["path"],
+        "tokenizer.json",
+        revision=rev,
+        use_auth_token=access_token,  # Pass the access token
+        local_files_only=True,
     )
     tokenizer = Tokenizer.from_file(tok_config)
 
@@ -49,28 +49,28 @@ def initialize_tokenizer(model_type, model_name):
 
     return tokenizer
 
-
 def initialize_model(model_type, model_name, tokenizer_only=False):
     model_info = get_model_info(model_type)
 
     allowed = ["*.bin", "*.txt", "*.json"]
     rev = model_info.get("revision", None)
 
-    # snapshot_download checks for updates by default
-    # This can cause significant lag in offline usecases or high latency networks
-    # To avoid this penalty, we try to use the local cache first.
-    # If the files are not available, then we attempt a download
     try:
         path = snapshot_download(
             model_info["path"],
             max_workers=1,
             allow_patterns=allowed,
             revision=rev,
+            use_auth_token=access_token,  # Pass the access token
             local_files_only=True,
         )
     except FileNotFoundError:
         path = snapshot_download(
-            model_info["path"], max_workers=1, allow_patterns=allowed, revision=rev
+            model_info["path"],
+            max_workers=1,
+            allow_patterns=allowed,
+            revision=rev,
+            use_auth_token=access_token,  # Pass the access token
         )
 
     if tokenizer_only:
@@ -87,27 +87,8 @@ def initialize_model(model_type, model_name, tokenizer_only=False):
     else:
         return ctranslate2.Translator(path, config["device"], compute_type="int8")
 
-
 def get_model(model_type, tokenizer_only=False):
-    """Gets a model from the loaded model cache
-
-    If tokenizer_only, the model itself will not be (re)loaded
-
-    >>> tokenizer, model = get_model("instruct")
-    >>> type(tokenizer)
-    <class 'tokenizers.Tokenizer'>
-
-    >>> type(model)
-    <class 'ctranslate2._ext.Translator'>
-
-    >>> tokenizer, model = get_model("embedding")
-    >>> type(tokenizer)
-    <class 'tokenizers.Tokenizer'>
-
-    >>> type(model)
-    <class 'ctranslate2._ext.Encoder'>
-    """
-
+    """Gets a model from the loaded model cache"""
     model_name = config[f"{model_type}_model"]
 
     if config["max_ram"] < 4 and not tokenizer_only:
@@ -116,7 +97,6 @@ def get_model(model_type, tokenizer_only=False):
                 try:
                     modelcache[model][1].unload_model()
                 except AttributeError:
-                    # Encoder-only models can't be unloaded by ctranslate2
                     pass
 
     if model_name not in modelcache:
@@ -124,17 +104,14 @@ def get_model(model_type, tokenizer_only=False):
         tokenizer = initialize_tokenizer(model_type, model_name)
         modelcache[model_name] = (tokenizer, model)
     elif not tokenizer_only:
-        # Make sure model is loaded if we've never loaded it
         if not modelcache[model_name][1]:
             modelcache[model_name] = (
                 modelcache[model_name][0],
                 initialize_model(model_type, model_name),
             )
-        # Make sure the model is reloaded if we've unloaded it
         try:
             modelcache[model_name][1].load_model()
         except AttributeError:
-            # Encoder-only models can't be unloaded in ctranslate2
             pass
 
     return modelcache[model_name]
